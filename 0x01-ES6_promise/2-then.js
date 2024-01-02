afbba304e6d46a5c819a8ae5c2d51a34645a38bb
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-02 21:35:46 +0000

    task2.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
 export default async function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  });
-  return promise.catch(() => new Error());
+  }).catch(() => new Error());
 }
