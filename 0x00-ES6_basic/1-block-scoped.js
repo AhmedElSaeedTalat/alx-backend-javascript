59c41271787a06028cd0c63403d0a4ac40c88a48
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-02 19:09:04 +0000

    edit for unused vars.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
+    const task = true; // eslint-disable-line
+    const task2 = false; // eslint-disable-line
   }
 
   return [task, task2];
