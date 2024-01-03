16b55c74ccd19774c85317ec9724ad6aa389f271
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-03 01:17:55 +0000

    settle all

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises).then((res) => res);
+  return Promise.allSettled(promises).then((res) => {
+    const array = [];
+    res.forEach((item) => {
+      const dict = {};
+      dict.status = item.status;
+      dict.value = (item.status === 'fulfilled') ? item.value : dict.value = String(item.reason);
+      array.push(dict);
+    });
+    return array;
+  });
 }
