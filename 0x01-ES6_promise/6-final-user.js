32c89134692c6122aea813acebbc360b3c0b213c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-03 00:28:08 +0000

    task 6 settled all

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((res) => res);
+}
