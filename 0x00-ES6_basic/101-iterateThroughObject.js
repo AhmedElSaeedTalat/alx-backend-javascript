260baec71faeec1a61c6d4a5268310c8c34bdf76
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-03 18:16:38 +0000

    task 14

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const array = [];
+  for (const item of reportWithIterator) {
+    array.push(item);
+  }
+  return array.join('|');
+}
