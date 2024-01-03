90ae62b3c20b901cda08025f529d33f3acc6c46a
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-03 11:56:13 +0000

    advanced task async await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,19 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const object = {};
-  try {
-    uploadPhoto().then((res) => {
-      object.photo = res;
-    });
-  } catch (error) {
-    object.photo = null;
-  }
-  try {
-    createUser().then((res) => {
-      object.user = res;
-    });
-  } catch (error) {
-    object.user = null;
-  }
+  uploadPhoto().then((res) => {
+    object.photo = res;
+  }).catch(() => { object.photo = null; });
+  createUser().then((res) => {
+    object.user = res;
+  }).catch(() => { object.user = null; });
   return object;
 }
