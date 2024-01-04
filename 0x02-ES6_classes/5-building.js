98414901e60dee14d6877a0843527d3626ed92c6
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-04 18:12:18 +0000

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error('extending Building must override evacuationWarningMessage');
@@ -16,9 +13,6 @@ export default class Building {
   }
 
   set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
     this._sqft = sqft;
   }
 }
