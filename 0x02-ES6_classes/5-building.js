2c65d026caa824481998efe2e67cec76577ed8f1
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-04 18:11:08 +0000

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -14,4 +14,11 @@ export default class Building {
   get sqft() {
     return this._sqft;
   }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
 }
