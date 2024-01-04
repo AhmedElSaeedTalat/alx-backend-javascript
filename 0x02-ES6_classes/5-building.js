23d8294c1b4ffbd388e3a971299099bd16dd42a9
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-04 18:05:41 +0000

    task 5 enforce function

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    if (typeof this.constructor !== 'Building') {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('extending Building must override evacuationWarningMessage');
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
