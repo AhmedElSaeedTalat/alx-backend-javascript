8a7c007b1dccd790c3dd996bc2a1d59419110298
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-08 18:45:15 +0000

    task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  if (position > buffer.length) {
+    throw new Error('Position outside range');
+  }
+  view[position] = value;
+  return buffer;
+}
