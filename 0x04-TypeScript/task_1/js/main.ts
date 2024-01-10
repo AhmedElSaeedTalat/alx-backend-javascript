ac7dd814884fea48a86e4bd73b479f3aedb151a7
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-10 17:21:17 +0000

    Task 2 extend interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,6 +6,11 @@ interface Teacher {
   location: string,
   [index:string]: any,
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -15,3 +20,12 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
