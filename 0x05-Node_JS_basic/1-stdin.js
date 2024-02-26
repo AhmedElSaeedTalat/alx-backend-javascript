bf2d12f4af6405accb728481d17689b12532a931
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-26 18:32:00 +0000

    t1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
   if (data) {
     process.stdout.write(`Your name is:  ${data}`);
   }
