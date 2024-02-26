f803496a758c995f1489ac587f3e603bc343af04
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-26 18:10:08 +0000

    t1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  console.log(`Your name is:  ${data}`);
+  process.exit();
+});
+process.on('end', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
