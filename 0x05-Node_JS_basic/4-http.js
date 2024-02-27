b02f0d12a9d4a121f2e313c7b76de2cb4245415c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-27 11:25:20 +0000

    t4 create server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.write('Hello Holberton School!');
   res.end();
 }).listen(1245);
