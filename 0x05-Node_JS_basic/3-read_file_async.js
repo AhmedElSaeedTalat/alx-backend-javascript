1d3eff8b968eaaeb312bb20f693110521f60c5a7
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-27 10:31:17 +0000

    t3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,60 @@
+const fs = require('fs').promises;
+
+// function to parse csv file manually
+const parse = (file) => {
+  let list = file.split('\n');
+  const newList = [];
+  let dict = {};
+  let head;
+  let row;
+  for (let i = 0; i < list.length; i += 1) {
+    if (i === 0) {
+      head = list[0].split(',');
+    } else {
+      row = list[i].split(',');
+      if (list[i]) {
+        for (let j = 0; j < head.length; j += 1) {
+          dict[head[j]] = row[j];
+        }
+        newList.push(dict);
+        dict = {};
+      }
+    }
+  }
+  list = [];
+  return newList;
+};
+
+//  function to count student
+//  enrolled in each filed
+const countFields = (list) => {
+  const dict = {};
+  for (let i = 0; i < list.length; i += 1) {
+    const { field } = list[i];
+    if (field) {
+      if (dict[field]) {
+        dict[field].push(list[i].firstname);
+      } else {
+        dict[field] = [];
+        dict[field].push(list[i].firstname);
+      }
+    }
+  }
+  for (const [key, value] of Object.entries(dict)) {
+    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+  }
+};
+
+const countStudents = async (path) => {
+  try {
+    const file = await fs.readFile(path, 'utf-8');
+    const parsedList = parse(file);
+    const count = parsedList.length;
+    console.log(`Number of students: ${count}`);
+    countFields(parsedList);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
